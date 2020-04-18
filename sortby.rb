83e11607c3ec27ca4e6f3b8fc91cecd940cb9a2d
Miguel <website.dev@icloud.com>
2020-04-18 16:30:36 -0500

    Added funtion to sort with yeild

--- sortby.rb
+++ sortby.rb
@@ -17,5 +17,24 @@ def sorting(array)
   [!sorted, array]
 end
 
+def bubble_sort_by(array)
+  items_unsorted = true
+  while items_unsorted
+    items_unsorted = false
+    (0...(array.count - 1)).each do |index|
+      if yield(array[index], array[index + 1]).negative?
+        items_unsorted = true
+        array[index], array[index + 1] = [array[index + 1], array[index]]
+      end
+    end
+  end
+  array
+end
+
 p 'Number Sort'
 puts bubble_sort_items([1, 2, 5, 4, 1, 7, 4, 7])
+p 'Alphabetical Sorting'
+puts bubble_sort_items(%w[a b c alpha charlie bravo])
+p 'String Size Sorting'
+array = %w[wazzup wazzzzup wazzzup wazzzzzzup wazup wazzzzzup]
+puts bubble_sort_by(array) { |item, size| item.length - size.length }
