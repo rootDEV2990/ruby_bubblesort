51d7a8c286cd0c47e772253f8de8c1a0da352187
rootDEV2990 <website.dev@icloud.com>
2020-04-23 11:11:20 -0500

    Finally sorts! =)

--- sortby.rb
+++ sortby.rb
@@ -1,11 +1,24 @@
 array = [5,3,4,9,1,8]
-x =  0
-loop_size = array.lenth
+
+
+#getting the size of items from user
+loop_size = array.length
+#starting loop
 loop do 
-  (loop_size-1).times do |x|
-    while array[x] > array[x+1]
-      array[x], array[x+1] = array[x+1], array[x]
-      x+=1 
+  #set bootlean to false
+  swap = false
+  #setting loop times to length - 1 loop is not needed in last number
+  #with each i in array do if or break loop
+  (loop_size-1).times do |i|
+    #if array item is greater than its neighbor = true
+    if array[i] > array[i + 1]
+      #change order of items
+      array[i], array[i + 1] = array[i + 1], array[i]
+      #while this code runs swap will have a true value
+      swap = true
     end
   end
+  #breat if not swap
+  break if !swap
+end
 p array
