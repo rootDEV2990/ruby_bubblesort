77afa0e66e4b75d3ef5fbe5c2a2e63dcc043e087
rootDEV2990 <website.dev@icloud.com>
2020-04-28 09:11:38 -0500

    Testing with yield

--- sortby.rb
+++ sortby.rb
@@ -17,7 +17,7 @@ def bubble_sort(array)
       end
     end
     #breat if not swap
-    break if !swap
+    break unless swap
   end
   array
 end  
@@ -25,3 +25,27 @@ end
 
 input = [5,3,4,9,1,8]
 p bubble_sort(input)
+
+
+def bubble_sort_by(array)
+  array = self.to_a
+  loop_size = array.length
+  loop do
+    swap = false
+    (loop_size-1).times do |i|
+      #if array item is greater than its neighbor = true
+      if yield (array[i], array[i + 1]) > 0
+        #change order of items
+        array[i], array[i + 1] = array[i + 1], array[i]
+        #while this code runs swap will have a true value
+        swap = true
+      end
+    end
+  end
+  array  
+end
+
+inp = [2,5,8,4,3,1]
+p bubble_sort_by(inp) do |fisrt, second| 
+  fisrt.length - second.length 
+end
