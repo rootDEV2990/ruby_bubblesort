88bd379ec68a82e88a22e76732122114d6a0ad86
rootDEV2990 <website.dev@icloud.com>
2020-04-28 18:48:00 -0500

    Fixed sticklers.

--- sortby.rb
+++ sortby.rb
@@ -21,7 +21,7 @@ def bubble_sort_by(array)
   loop do
     swap = false
     (loop_size - 1).times do |i|
-      next unless yield(array[i], array[i + 1]) > 0
+      if yield(array[i], array[i + 1]) > 0
 
       array[i], array[i + 1] = array[i + 1], array[i]
       swap = true
