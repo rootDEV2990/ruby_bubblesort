bf8697dbfb7cf5c7a837f3900059eecac6fdcf84
rootDEV2990 <website.dev@icloud.com>
2020-04-28 18:41:44 -0500

    Fixed bubble sorty by method

--- sortby.rb
+++ sortby.rb
@@ -1,51 +1,53 @@
 def bubble_sort(array)
-  #getting the size of items from user
+  # getting the size of items from user
   loop_size = array.length
-  #starting loop
-  loop do 
-    #swap sets to false is not yet running
+  # starting loop
+  loop do
+    # swap sets to false is not yet running
     swap = false
-    #setting loop times to length - 1 loop is not needed in last number
-    #with each i in array do if or break loop
-    (loop_size-1).times do |i|
-      #if array item is greater than its neighbor = true
-      if array[i] > array[i + 1]
-        #change order of items
-        array[i], array[i + 1] = array[i + 1], array[i]
-        #while this code runs swap will have a true value
-        swap = true
-      end
+    # setting loop times to length - 1 loop is not needed in last number
+    # with each i in array do if or break loop
+    (loop_size - 1).times do |i|
+      # if array item is greater than its neighbor = true
+      next unless array[i] > array[i + 1]
+
+      # change order of items
+      array[i], array[i + 1] = array[i + 1], array[i]
+      # while this code runs swap will have a true value
+      swap = true
     end
-    #breat if not swap
+    # breat if not swap
     break unless swap
   end
   array
-end  
+end
 
-
-input = [5,3,4,9,1,8]
+input = [5, 3, 4, 9, 1, 8]
 p bubble_sort(input)
 
-
 def bubble_sort_by(array)
-  array = self.to_a
+  # getting the size of items from user
   loop_size = array.length
+  # starting loop
   loop do
+    # swap sets to false is not yet running
     swap = false
-    (loop_size-1).times do |i|
-      #if array item is greater than its neighbor = true
-      if yield (array[i], array[i + 1]) > 0
-        #change order of items
-        array[i], array[i + 1] = array[i + 1], array[i]
-        #while this code runs swap will have a true value
-        swap = true
-      end
+    # setting loop times to length - 1 loop is not needed in last number
+    # with each i in array do if or break loop
+    (loop_size - 1).times do |i|
+      # if array item is greater than its neighbor = true
+      next unless yield(array[i], array[i + 1]) > 0
+
+      # change order of items
+      array[i], array[i + 1] = array[i + 1], array[i]
+      # while this code runs swap will have a true value
+      swap = true
     end
+    # breat if not swap
+    break unless swap
   end
-  array  
+  array
 end
 
-inp = [2,5,8,4,3,1]
-p bubble_sort_by(inp) do |fisrt, second| 
-  fisrt.length - second.length 
-end
+inp = [1, 3, 4, 5, 8, 9]
+p bubble_sort_by(inp) { |first, second| second - first }
