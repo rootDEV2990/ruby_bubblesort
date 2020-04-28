49f55f854aa0b26982266a651b9e3aefc774f502
rootDEV2990 <website.dev@icloud.com>
2020-04-28 18:45:40 -0500

    Removed comments

--- sortby.rb
+++ sortby.rb
@@ -1,22 +1,13 @@
 def bubble_sort(array)
-  # getting the size of items from user
   loop_size = array.length
-  # starting loop
   loop do
-    # swap sets to false is not yet running
     swap = false
-    # setting loop times to length - 1 loop is not needed in last number
-    # with each i in array do if or break loop
     (loop_size - 1).times do |i|
-      # if array item is greater than its neighbor = true
       next unless array[i] > array[i + 1]
 
-      # change order of items
       array[i], array[i + 1] = array[i + 1], array[i]
-      # while this code runs swap will have a true value
       swap = true
     end
-    # breat if not swap
     break unless swap
   end
   array
@@ -26,24 +17,15 @@ input = [5, 3, 4, 9, 1, 8]
 p bubble_sort(input)
 
 def bubble_sort_by(array)
-  # getting the size of items from user
   loop_size = array.length
-  # starting loop
   loop do
-    # swap sets to false is not yet running
     swap = false
-    # setting loop times to length - 1 loop is not needed in last number
-    # with each i in array do if or break loop
     (loop_size - 1).times do |i|
-      # if array item is greater than its neighbor = true
       next unless yield(array[i], array[i + 1]) > 0
 
-      # change order of items
       array[i], array[i + 1] = array[i + 1], array[i]
-      # while this code runs swap will have a true value
       swap = true
     end
-    # breat if not swap
     break unless swap
   end
   array
