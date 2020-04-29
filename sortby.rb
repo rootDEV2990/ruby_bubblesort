fe8e22298607dcb7a49c0987125f646a4375126c
rootDEV2990 <website.dev@icloud.com>
2020-04-29 14:48:39 -0500

    Fixed rubocop errors

--- sortby.rb
+++ sortby.rb
@@ -3,11 +3,10 @@ def bubble_sort(array)
   loop do
     swap = false
     (loop_size - 1).times do |i|
-      if array[i] > array[i + 1]
+      next unless array[i] > array[i + 1]
 
       array[i], array[i + 1] = array[i + 1], array[i]
       swap = true
-      end
     end
     break unless swap
   end
@@ -19,21 +18,12 @@ def bubble_sort_by(array)
   loop do
     swap = false
     (loop_size - 1).times do |i|
-      if yield(array[i], array[i + 1]) > 0
+      next unless yield(array[i], array[i + 1]) > 0
 
       array[i], array[i + 1] = array[i + 1], array[i]
       swap = true
-      end
     end
     break unless swap
   end
   array
 end
-
-=begin test cases
-input = [5, 3, 4, 9, 1, 8]
-p bubble_sort(input)
-
-inp = [1, 3, 4, 5, 8, 9]
-p bubble_sort_by(inp) { |first, second| second - first }
-=end
